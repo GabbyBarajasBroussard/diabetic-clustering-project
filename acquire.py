1cd3091a35356b7cdf95c654c4e04e6804c04ea8
Gabby Broussard <gabriella.b.broussard@gmail.com>
2021-04-09 10:31:57 -0500

    updates

--- acquire.py
+++ acquire.py
@@ -1,58 +1,25 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pandas as pd\n",
-    "import numpy as np\n",
-    "import os\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def get_diabetic_data():\n",
-    "    df= pd.read_csv('diabetic_data.csv', index_col=0)\n",
-    "    return df"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def get_id_data():\n",
-    "    df= pd.read_csv('IDs_mapping.csv', index_col=0)\n",
-    "    return df"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.6"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import os
+
+
+# In[ ]:
+
+
+def get_diabetic_data():
+    df= pd.read_csv('diabetic_data.csv', index_col=0)
+    return df
+
+
+# In[ ]:
+
+
+def get_id_data():
+    df= pd.read_csv('diabetic_data.csv', index_col=0)
+
